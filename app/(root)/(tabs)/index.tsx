36b50758e8779175572baaf947340f1b2ffa7e08
0ALEX-2 <sakilahmed965@gmail.com>
2025-01-18 22:12:45 +0530

    sign in page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold">Welcome to Real-state </Text>
+      <Text className="font-bold font-rubik">Welcome to Real-state</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
